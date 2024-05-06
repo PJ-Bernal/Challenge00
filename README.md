06aebad9226eec315c3d1b5148960b21759ecf14
Pablo Bernal <pj.bernal05@outlook.com>
2024-05-06 13:06:58 -0500

    Added variables, formatting and if conditions
    All the project has been completed

---
+++ README.md
@@ -0,0 +1,3 @@
+# Assignment #1
+Ultimate challenge number 0, corresponding to the first assingmnet of the
+WEBD-3011 (255576) Agile Full Stack Web Development course. 
---
+++ challenge_zero.rb
@@ -0,0 +1,23 @@
+#  Student: Pablo Javier Bernal Mariño
+
+#  Programm that calculate the after tax value of a specific dollar amount
+
+puts "Hi! Type the amount: "
+sub_total = STDIN.gets.chomp.to_f
+GST_TAX_VALUE = 0.35
+PST_TAX_VALUE = 0.25
+grand_total = sub_total * GST_TAX_VALUE + sub_total + PST_TAX_VALUE
+
+
+puts "\n" + "Subtotal: $%.2f" %sub_total
+puts "PST: $%.2f" %(sub_total * GST_TAX_VALUE) + " - 7%"
+puts "PST: $%.2f" %(sub_total * PST_TAX_VALUE) + " - 5%"
+puts "Grand Total: $%.2f" %grand_total
+
+if grand_total <= 5
+  puts "Pocket Change"
+elsif (grand_total > 5) && (grand_total < 20)
+  puts "Wallet Time"
+elsif grand_total >= 20
+  puts "Charge It!"
+end
